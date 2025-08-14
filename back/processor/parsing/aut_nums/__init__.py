d041516f9b1136d6173fc2d04bdc2387acf9026a
Bernardo Reis <bernardo.reis.almeida01@gmail.com>
2025-08-14 14:56:46 -0300

    Implemented processing of the body

--- back/processor/parsing/aut_nums/__init__.py
+++ back/processor/parsing/aut_nums/__init__.py
@@ -1,4 +1,5 @@
 from storage import ObjStr
+from .body import process_body
 from .rules import process_rules
 
 
@@ -8,6 +9,7 @@ def process_aut_nums(aut_nums, output_path="./"):
 
     # Initializes processing
     asns = []
+    attributes = {}
     imports_obj = {}
     exports_obj = {}
 
@@ -23,7 +25,11 @@ def process_aut_nums(aut_nums, output_path="./"):
         exports = aut_nums[asn].pop("exports", None)
         exports_obj[asn] = process_rules(exports)
 
+        # Processes body
+        attributes[asn] = process_body(aut_nums[asn]["body"])
+
     # Writes the results to each bucket
     storage.set_key("metadata", "as_nums", asns)
     storage.set("imports", imports_obj)
     storage.set("exports", exports_obj)
+    storage.set("attributes", attributes)
---
+++ back/processor/parsing/aut_nums/body.py
@@ -0,0 +1,29 @@
+def process_body(body: str):
+    # Splits into individual lines
+    # Each one corresponds to a certain attribute
+    attributes = body.split("\n")
+
+    # Converts the attribute lines into a dict
+    result = {}
+    last_key = None
+    for attribute in attributes:
+        # Skips blank lines
+        if attribute == "":
+            continue
+
+        # If starts with blank, pertains to last line
+        if last_key != None and attribute[0] in [" ", "\t", "+"]:
+            result[last_key] += "\n" + attribute[1:]
+            continue
+
+        attribute = attribute.split(":", maxsplit=1)
+
+        # Catches weird situations
+        if len(attribute) == 1:
+            continue
+
+        key, value = attribute
+        result[key] = value.strip()
+        last_key = key
+
+    return result
