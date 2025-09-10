56c9a9db7b37ac5b317fed21a5b6e50384c7df42
Bernardo Reis <bernardo.reis.almeida01@gmail.com>
2025-09-10 11:02:33 -0300

    Small fixes on data processor

--- data/processor/parsing/as_routes/__init__.py
+++ data/processor/parsing/as_routes/__init__.py
@@ -7,16 +7,16 @@ def process_as_routes(as_routes, storage):
     as_routes = {k: {"routes": v} for k, v in as_routes.items()}
 
     # Processes inverted version
-    addr_prefixes = list(as_routes_inverted.keys())
     announced_by = process_announced_by(as_routes)
+    addr_prefixes = list(announced_by.keys())
     announcement = process_announcement(as_routes, announced_by)
 
     # Persists results
     storage.set_key("metadata", "addr_prefixes", addr_prefixes)
-    del prefixes
+    del addr_prefixes
     storage.set("addr-announces", as_routes)
     del as_routes
     storage.set("addr-announced_by", announced_by)
-    del as_routes_inverted
+    del announced_by
     storage.set("addr-announcement", announcement)
     del announcement
